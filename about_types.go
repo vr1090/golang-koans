840ecb7c8710901863f585dbd55b35e3e49866a3
inu.pragantha <vr1090@gmail.com>
2025-01-06 20:46:16 +0700

    update code 06d-mm-YYYY

--- about_types.go
+++ about_types.go
@@ -1,13 +1,20 @@
 package go_koans
 
+import "fmt"
+
 type coolNumber int
 
+type babiGila coolNumber
+
 func (cn coolNumber) multiplyByTwo() int {
+	uhui := babiGila(cn)
+	fmt.Print(uhui)
+
 	return int(cn) * 2
 }
 
 func aboutTypes() {
 	i := coolNumber(4)
-	assert(i == coolNumber(__int__))     // values can be converted between compatible types
-	assert(i.multiplyByTwo() == __int__) // you can add methods on any type you define
+	assert(i == coolNumber(4))     // values can be converted between compatible types
+	assert(i.multiplyByTwo() == 8) // you can add methods on any type you define
 }
