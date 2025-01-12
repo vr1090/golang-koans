554a45f2f7868be0bb613726dcf17030ad652034
inu.pragantha <vr1090@gmail.com>
2025-01-12 07:10:35 +0700

    update code 12d-mm-YYYY

--- about_panics.go
+++ about_panics.go
@@ -1,14 +1,16 @@
 package go_koans
 
 func divideFourBy(i int) int {
+
 	return 4 / i
 }
 
-const __divisor__ = 0
+const __divisor__ = 2
 
 func aboutPanics() {
-	assert(__delete_me__) // panics are exceptional errors at runtime
+	assert(true) // panics are exceptional errors at runtime
 
 	n := divideFourBy(__divisor__)
+	
 	assert(n == 2) // panics are exceptional errors at runtime
 }
